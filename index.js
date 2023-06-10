70d06b176a85e2755fdc52779cd107ec51b8df34
Akram-Ul-Hasan <akramulhasan66@gmail.com>
2023-06-10 20:41:48 +0600

    added classes get api

--- index.js
+++ index.js
@@ -1,49 +1,60 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = process.env.PORT || 5000;
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-
-
-
+// mongodb connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8bdsz2a.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const classCollection = client
+      .db("language-safari-db")
+      .collection("classes");
+    const userCollection = client.db("language-safari-db").collection("users");
+    const instructorCollection = client.db("language-safari-db").collection("instructor");
+
+
+    
+
+    // classes crud operation
+    app.get("/classes", async (req, res) => {
+      const result = await classCollection.find().toArray();
+      res.send(result);
+    });
+
+   
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-    // Ensures that the client will close when you finish/error
-    //await client.close();
   }
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Language safari server is running");
+});
 
-
-
-app.get('/', (req,res)=>{
-    res.send('Language safari server is running');
-})
-
-app.listen(port,()=>{
-    console.log(`Language safari is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Language safari is running on port ${port}`);
+});

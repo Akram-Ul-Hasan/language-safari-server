4746cf3d93f0e98cc188718ab46606948e73d728
Akram-Ul-Hasan <akramulhasan66@gmail.com>
2023-06-14 21:39:05 +0600

    added a sslcommerz payment api

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-
+const SSLCommerzPayment = require("sslcommerz-lts");
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const store_id = process.env.STORE_ID;
+const store_passwd = process.env.STORE_PASS;
+const is_live = false;
 
 // mongodb connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8bdsz2a.mongodb.net/?retryWrites=true&w=majority`;
@@ -36,7 +40,7 @@ async function run() {
     // cart
     app.get("/carts", async (req, res) => {
       const email = req.query.email;
-      
+
       if (!email) {
         res.send([]);
       }
@@ -76,9 +80,12 @@ async function run() {
     app.get("/instructors", async (req, res) => {
       let result;
       if (req.query?.search) {
-        result = await instructorCollection.find().sort({ student: 1 }).limit(6).toArray();
-      } 
-      else {
+        result = await instructorCollection
+          .find()
+          .sort({ student: 1 })
+          .limit(6)
+          .toArray();
+      } else {
         result = await instructorCollection.find().toArray();
       }
       res.send(result);
@@ -86,15 +93,62 @@ async function run() {
 
     // classes crud operation
     app.get("/classes", async (req, res) => {
-      
       let result;
       if (req.query?.search) {
-        result = await classCollection.find().sort({ student: 1 }).limit(6).toArray();
+        result = await classCollection
+          .find()
+          .sort({ student: 1 })
+          .limit(6)
+          .toArray();
       } else {
         result = await classCollection.find().toArray();
       }
 
       res.send(result);
+    });
+
+    // order
+    const tran_id = new ObjectId().toString();
+    app.post("/order", async (req, res) => {
+      const order = req.body;
+      console.log(order);
+      const data = {
+        total_amount: order.amount,
+        currency: "BDT",
+        tran_id: tran_id, // use unique tran_id for each api call
+        success_url: "http://localhost:3030/success",
+        fail_url: "http://localhost:3030/fail",
+        cancel_url: "http://localhost:3030/cancel",
+        ipn_url: "http://localhost:3030/ipn",
+        shipping_method: "Courier",
+        product_name: "Computer.",
+        product_category: "Electronic",
+        product_profile: "general",
+        cus_name: order.name,
+        cus_email: order.email,
+        cus_add1: "Dhaka",
+        cus_add2: "Dhaka",
+        cus_city: "Dhaka",
+        cus_state: "Dhaka",
+        cus_postcode: "1000",
+        cus_country: "Bangladesh",
+        cus_phone: "01711111111",
+        cus_fax: "01711111111",
+        ship_name: "Customer Name",
+        ship_add1: "Dhaka",
+        ship_add2: "Dhaka",
+        ship_city: "Dhaka",
+        ship_state: "Dhaka",
+        ship_postcode: 1000,
+        ship_country: "Bangladesh",
+      };
+      const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live);
+      sslcz.init(data).then((apiResponse) => {
+        // Redirect the user to payment gateway
+        let GatewayPageURL = apiResponse.GatewayPageURL;
+        res.send({url:GatewayPageURL});
+        console.log("Redirecting to: ", GatewayPageURL);
+      });
     });
 
     // Send a ping to confirm a successful connection

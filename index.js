0d941dc862c1b9420ac37c2773dd1cbc571a7a96
Akram-Ul-Hasan <akramulhasan66@gmail.com>
2023-06-14 20:21:14 +0600

    added delete api for carts

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
 app.use(cors());
@@ -34,8 +34,9 @@ async function run() {
     const cartCollection = client.db("language-safari-db").collection("carts");
 
     // cart
-    app.get("carts", async (req, res) => {
-      const email = req.body.email;
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      
       if (!email) {
         res.send([]);
       }
@@ -45,9 +46,17 @@ async function run() {
     });
 
     app.post("/carts", async (req, res) => {
-      const newCart = req.body;
-      console.log(newCart);
-      const result = await cartCollection.insertOne(newCart);
+      const newItem = req.body;
+
+      const result = await cartCollection.insertOne(newItem);
+      res.send(result);
+    });
+
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
 
